061b15f742a201bbada28b373ffc5765ff9958aa
1216083447 <1216083447@qq.com>
2015-12-30 15:29:20 +0800

    构建openstack-dashboard liberty

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,24 @@
-# image name lzh/dashboard:kilo
-FROM registry.lzh.site:5000/lzh/openstackbase:kilo
+# image name lzh/dashboard:liberty
+FROM 10.64.0.50:5000/lzh/openstackbase:liberty
 
 MAINTAINER Zuhui Liu penguin_tux@live.com
 
-ENV BASE_VERSION 2015-07-17
-ENV OPENSTACK_VERSION kilo
+ENV BASE_VERSION 2015-12-28
+ENV OPENSTACK_VERSION liberty
+ENV BUID_VERSION 2015-12-30
 
+RUN yum update -y && \
+         yum install -y openstack-dashboard && \
+         rm -rf /var/cache/yum/*
 
-ENV DEBIAN_FRONTEND noninteractive
-
-RUN apt-get update
-RUN apt-get dist-upgrade -y
-RUN apt-get install openstack-dashboard apache2 -y
-RUN apt-get clean
-
-RUN env --unset=DEBIAN_FRONTEND
+RUN cp -rp /etc/nova/ /nova && \
+         rm -rf /etc/nova/* && \
+         rm -rf /var/log/nova/*
 
 ADD entrypoint.sh /usr/bin/entrypoint.sh
 RUN chmod +x /usr/bin/entrypoint.sh
 
-ADD dashboard.conf /etc/supervisor/conf.d/dashboard.conf
+ADD dashboard.ini /etc/supervisord.d/dashboard.ini
 
 RUN cp -rp /etc/openstack-dashboard/ /openstack-dashboard
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -17,24 +17,47 @@ fi
 
 if [ ! -f /etc/openstack-dashboard/.complete ];then
     cp -rp /openstack-dashboard/* /etc/openstack-dashboard/
-    sed -i /OPENSTACK_HOST/s/127.0.0.1/$KEYSTONE_ENDPOINT/ /etc/openstack-dashboard/local_settings.py
     
-    grep ^ALLOWED_HOSTS >/dev/null 2>/dev/null /etc/openstack-dashboard/local_settings.py && sed -i /^ALLOWED_HOSTS/d /etc/openstack-dashboard/local_settings.py
-    echo ALLOWED_HOSTS = [\'*\', ] >> /etc/openstack-dashboard/local_settings.py
+    grep ^OPENSTACK_HOST /etc/openstack-dashboard/local_settings >/dev/null 2>/dev/null
+    if [ $? -eq 0 ];then
+      sed -i "/^OPENSTACK_HOST/c\OPENSTACK_HOST = \"$KEYSTONE_ENDPOINT\"" /etc/openstack-dashboard/local_settings
+    else
+      echo 'OPENSTACK_HOST = "$KEYSTONE_ENDPOINT" ' >> /etc/openstack-dashboard/local_settings
+    fi
     
-    sed -i /^CACHES/,/^}/d /etc/openstack-dashboard/local_settings.py
-    echo SESSION_ENGINE = \'django.contrib.sessions.backends.cache\' >> /etc/openstack-dashboard/local_settings.py
-    echo CACHES = { >> /etc/openstack-dashboard/local_settings.py
-    echo \ \ \'default\':\ { >> /etc/openstack-dashboard/local_settings.py
-    echo \ \ \ \ \'BACKEND\': \'django.core.cache.backends.memcached.MemcachedCache\', >> /etc/openstack-dashboard/local_settings.py
-    echo \ \ \ \ \'LOCATION\': \'$MEMCACHE_SERVER:11211\', >> /etc/openstack-dashboard/local_settings.py
-    echo \ \ } >> /etc/openstack-dashboard/local_settings.py
-    echo } >> /etc/openstack-dashboard/local_settings.py
+    grep ^ALLOWED_HOSTS  /etc/openstack-dashboard/local_settings >/dev/null 2>/dev/null
+    if [ $? -eq 0 ];then
+      sed -i "/^ALLOWED_HOSTS/c\ALLOWED_HOSTS = ['*', ]"  /etc/openstack-dashboard/local_settings
+    else
+      echo ALLOWED_HOSTS = [\'*\', ] >> /etc/openstack-dashboard/local_settings
+    fi
+
+    sed -i /^CACHES/,/^}/d /etc/openstack-dashboard/local_settings
+    echo SESSION_ENGINE = \'django.contrib.sessions.backends.cache\' >> /etc/openstack-dashboard/local_settings
+    echo CACHES = { >> /etc/openstack-dashboard/local_settings
+    echo \ \ \'default\':\ { >> /etc/openstack-dashboard/local_settings
+    echo \ \ \ \ \'BACKEND\': \'django.core.cache.backends.memcached.MemcachedCache\', >> /etc/openstack-dashboard/local_settings
+    echo \ \ \ \ \'LOCATION\': \'$MEMCACHE_SERVER:11211\', >> /etc/openstack-dashboard/local_settings
+    echo \ \ } >> /etc/openstack-dashboard/local_settings
+    echo } >> /etc/openstack-dashboard/local_settings
+
+    grep ^OPENSTACK_KEYSTONE_DEFAULT_ROLE /etc/openstack-dashboard/local_settings >/dev/null 2>/dev/null
+    if [ $? -eq 0 ];then
+      sed -i "/^OPENSTACK_KEYSTONE_DEFAULT_ROLE/c\OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"$OPENSTACK_KEYSTONE_DEFAULT_ROLE\" "  /etc/openstack-dashboard/local_settings
+      sed -i /^ALLOWED_HOSTS/d /etc/openstack-dashboard/local_settings
+    else
+     echo "OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"$OPENSTACK_KEYSTONE_DEFAULT_ROLE\"" >> /etc/openstack-dashboard/local_settings
+    fi    
     
-    sed -i /OPENSTACK_KEYSTONE_DEFAULT_ROLE/s/_member_/$OPENSTACK_KEYSTONE_DEFAULT_ROLE/g  /etc/openstack-dashboard/local_settings.py
-    sed -i '/TIME_ZONE/s/UTC/Asia\/Chongqing/g'  /etc/openstack-dashboard/local_settings.py
+    grep ^TIME_ZONE /etc/openstack-dashboard/local_settings >/dev/null 2>/dev/null
+    if [ $? -eq 0 ];then
+    sed -i '/TIME_ZONE/c\TIME_ZONE = "Asia/Chongqing"'  /etc/openstack-dashboard/local_settings
+    else
+      echo 'TIME_ZONE = "Asia/Chongqing"' >> /etc/openstack-dashboard/local_settings
+    fi
     
     touch /etc/openstack-dashboard/.complete
+
 fi
 
 /usr/bin/supervisord -n
